ef7757758d4f9dc5484736b39c5af941c29ecdfd
Izi Martin <39589537+Izimartin@users.noreply.github.com>
2022-04-12 14:14:28 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,66 +1,51 @@
 #include "main.h"
 
-int _putchar(char c);
-
 /**
- * print_times_table - print the times table until n
- *
- * @n: n times table
- *
- * Return: No return
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
  */
 
 void print_times_table(int n)
 {
-	int i;
-	int j;
 
-	if (n >= 0 && n <= 15)
+int a = 0, rep, b;
+
+if (n < 0 || n > 15)
+	return;
+
+while (a <= n)
+{
+	for (b = 0; b <= n; b++)
 	{
-
-		for (i = 0; i <= n; i++)
+		rep = a * b;
+		if (b == 0)
+			_putchar('0' + rep);
+		else if (rep < 10)
 		{
-
-			for (j = 0; j <= n; j++)
-			{
-
-				if ((i * j) < 10)
-				{
-					if (j != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar((j * i) + '0');
-				}
-
-				else if ((i * j) < 100)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((((j * i) / 10) % 10) + '0');
-					_putchar(((j * i) % 10) + '0');
-				}
-
-				else if ((i * j) < 1000)
-				{
-					_putchar(' ');
-					_putchar((((j * i) / 100) % 10) + '0');
-					_putchar((((j * i) / 10) % 10) + '0');
-					_putchar(((j * i) % 10) + '0');
-				}
-			if (j != (n))
-			{
-				_putchar(',');
-			}
-
-			}
-
-		_putchar('\n');
-
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + rep);
 		}
-
+		else if (rep < 100)
+		{
+			_putchar(' ');
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+		else
+		{
+			_putchar('0' + rep / 100);
+			_putchar('0' + (rep - 100) / 10);
+			_putchar('0' + rep % 10);
+		}
+		if (b < n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
-
+	_putchar('\n');
+	a++;
+}
 }
